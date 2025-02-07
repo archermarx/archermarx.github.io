faeb098bf4b610d49f76757381f1c13f8710dd91
Thomas Marks <marksta@umich.edu>
2025-02-06 22:04:13 -0500

    use my own template instead of auto-generated this simplifies the python stuff nicely

--- md/make.py
+++ md/make.py
@@ -20,13 +20,18 @@ reldir = "../" * depth
 with open(mdfile, "r") as file:
     contents = file.readlines()
 
-# find YAML header
+# ========= Insert date into YAML header if not present ===================
 div_count = 0
 div_ind = 0
 title_ind = 0
+has_date = False
 for i, l in enumerate(contents):
-    if l.startswith("Title"):
+    lower = l.casefold()
+    if lower.startswith("title") or lower.startswith("pagetitle"):
         title_ind = i
+    if lower.startswith("date"):
+        print(lower)
+        has_date = True
     if l.strip() == "---":
         div_count += 1
         div_ind = i
@@ -36,69 +41,19 @@ for i, l in enumerate(contents):
 header = contents[: div_ind + 1]
 contents = contents[div_ind + 1 :]
 
-# add favicon into header after title
-favicon_path = "/favicon.ico"
-
-favicon_header = [
-    "header-includes:\n",
-    f'    <link rel="icon" type="image/x-icon" href="{favicon_path}"/>\n',
-]
-
-header = header[0 : title_ind + 1] + favicon_header + header[title_ind + 1 :]
-
-# ========= Write navbar ===================================================
-active = 'class="active"'
-is_home = active if mdfile == "index.md" else ""
-is_pubs = active if mdfile == "publications.md" else ""
-
-content_paths = {"p", "content"}
-
-if mdfile == "archive.md" or str(Path(mdfile).parent) in content_paths:
-    is_posts = active
+if not has_date:
+    date = datetime.strptime(time.ctime(os.path.getmtime(mdfile)), "%c")
+    date = date.strftime("%b %d, %Y")
+    date_header = [f"date: '{date}'\n"]
 else:
-    is_posts = ""
+    date_header = []
 
-navbar = f"""
-<div class="navbar" id="navigation_bar">
-<a {is_home} href="/">Home</a>
-<a {is_pubs} href="/publications">Publications</a>
-<a {is_posts} href="/archive">Posts</a>
-<a href="/files/cv.pdf">Curriculum Vitae</a>
-<a href="javascript:void(0);" class="icon" onclick="responsive_navbar()">
-    <i class="fa fa-bars"></i>
-</a>
-</div>
-"""
-
-navbar_js = """
-<script>
-function responsive_navbar() {
-  var x = document.getElementById("navigation_bar");
-  if (x.className === "navbar") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar";
-  }
-}
-</script>
-"""
-
-# ========= Write footer with modification date  ===========================
-date = datetime.strptime(time.ctime(os.path.getmtime(mdfile)), "%c")
-date = date.strftime("%b %d, %Y")
-
-footer = f"""\n\n\\ \n\n***\n
-<span class="footer">
-*Last updated on {date}. Created using [pandoc](http://pandoc.org/).
-</span>"""
+header = header[0 : title_ind + 1] + date_header + header[title_ind + 1 :]
 
 # ========= Write modified md/html to temporary file =======================
 with open(tempfile, "w") as file:
     file.writelines(header)
-    file.write(navbar)
     file.writelines(contents)
-    file.write(navbar_js)
-    file.write(footer)
 
 # ========= Convert markdown to html using pandoc ==========================
 print(htmlfile)
@@ -113,6 +68,8 @@ args = [
     "-t",
     "html5",
     "--standalone",
+    "--template",
+    f"../{assets}/html/template.html",
     "--css",
     f"{reldir}/{assets}/css/style.css",
     tempfile,
